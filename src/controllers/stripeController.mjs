608b1b8a92872dba303511c75a94eca40491d3e3
aaronr24468 <aaronr24468@gmail.com>
2026-07-18 14:26:33 -0600

    Comment stripe lines

--- src/controllers/stripeController.mjs
+++ src/controllers/stripeController.mjs
@@ -3,31 +3,31 @@ import { config } from "dotenv";
 import { AppError } from "../services/AppError.mjs";
 config();
 
-const stripe = new Stripe(process.env.STRIPE_SK);
+// const stripe = new Stripe(process.env.STRIPE_SK);
 
-export const payment_Intent = async (request, response, next) => {
-    try {
-        const dataPayment = {
-            id_package: request.body.id_package,
-            price: request.body.price,
-            paymentIntentId: ''
-        }
+// export const payment_Intent = async (request, response, next) => {
+//     try {
+//         const dataPayment = {
+//             id_package: request.body.id_package,
+//             price: request.body.price,
+//             paymentIntentId: ''
+//         }
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: 100000,
-            currency: 'mxn',
-            metadata: {
-                id_package: dataPayment.id_package
-            }
-        })
+//         const paymentIntent = await stripe.paymentIntents.create({
+//             amount: 100000,
+//             currency: 'mxn',
+//             metadata: {
+//                 id_package: dataPayment.id_package
+//             }
+//         })
 
-        console.log(paymentIntent.client_secret)
-        console.log(paymentIntent.id)
-        dataPayment.paymentIntentId = paymentIntent.id;
+//         console.log(paymentIntent.client_secret)
+//         console.log(paymentIntent.id)
+//         dataPayment.paymentIntentId = paymentIntent.id;
 
-        response.json({ok: true, message: 'Success', clientSecret: paymentIntent.client_secret});
+//         response.json({ok: true, message: 'Success', clientSecret: paymentIntent.client_secret});
 
-    } catch (error) {
-        next(error)
-    }
-}
+//     } catch (error) {
+//         next(error)
+//     }
+// }
